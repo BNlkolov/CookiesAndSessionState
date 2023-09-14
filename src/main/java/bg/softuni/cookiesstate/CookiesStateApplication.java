908b0de999e9cbcfe69657a0b6cf56cf9565b226
Borislav Nikolov <borislavn441@gmail.com>
2023-09-14 18:22:44 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/cookiesstate/CookiesStateApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.cookiesstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiesStateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookiesStateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/cookiesstate/CookiesStateApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.cookiesstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiesStateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
